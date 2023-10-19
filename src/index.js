51de5f1a343e0a0de02f31348b386f665d46ec7a
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-19 11:26:21 -0300

    feat: add todo update done status

--- src/index.js
+++ src/index.js
@@ -74,7 +74,18 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  todo.done = true;
+
+  return response.status(201).send();
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
